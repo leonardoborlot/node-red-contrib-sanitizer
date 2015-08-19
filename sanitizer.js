681116b83fd9702f75938cfd10fd848cf40c7451
rmaheshkumarblr <mahesh.ravindranathan@colorado.edu>
2015-08-19 00:35:23 -0600

    Updated version of sanitizer.html and sanitizer.js

--- sanitizer.js
+++ sanitizer.js
@@ -1,33 +1,15 @@
-/**
- * Copyright 2014 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
-// If you use this as a template, update the copyright with your own name.
-
-// Sample Node-RED node file
-
-
 module.exports = function(RED) {
     "use strict";
-    // require any external libraries we may need....
-    //var foo = require("foo-library");
+    var util = require("util");
+
+
 
     // The main node definition - most things happen in here
-    function SampleNode(n) {
+    function SanitizerNode(n) {
         // Create a RED node
         RED.nodes.createNode(this,n);
+
+        this.optionSelected = n.optionSelected
 
         // Store local copies of the node configuration (as defined in the .html)
         this.topic = n.topic;
@@ -35,23 +17,71 @@ module.exports = function(RED) {
         // copy "this" object in case we need it in context of callbacks of other functions.
         var node = this;
 
-        // Do whatever you need to do in here - declare callbacks etc
-        // Note: this sample doesn't do anything much - it will only send
-        // this message once at startup...
-        // Look at other real nodes for some better ideas of what to do....
         var msg = {};
         msg.topic = this.topic;
-        msg.payload = "Hello world !";
+        //msg.payload = "Hello world !";
 
         // send out the message to the rest of the workspace.
         // ... this message will get sent at startup so you may not see it in a debug node.
-        this.send(msg);
+        //this.send(msg);
 
         // respond to inputs....
         this.on('input', function (msg) {
-            node.warn("I saw a payload: "+msg.payload);
+            //node.warn("I saw a payload: "+msg.payload);
             // in this example just send it straight on... should process it here really
-            node.send(msg);
+            try 
+            {
+                
+                if (msg.hasOwnProperty("payload")) 
+                {
+                    if (typeof msg.payload === "string") {
+                        try {
+                            msg.payload = JSON.parse(msg.payload);
+                            //node.send(msg);
+                        }
+                        catch(e) { node.error(e.message,msg); }
+                    }
+                    else if (typeof msg.payload === "object") {
+                        if ((!Buffer.isBuffer(msg.payload)) && (!util.isArray(msg.payload))) {
+                            msg.payload = JSON.stringify(msg.payload);
+                            //node.send(msg);
+                        }
+                        else { node.warn(RED._("json.errors.dropped-object")); }
+                    }
+                    else { node.warn(RED._("json.errors.dropped")); }
+                }
+
+
+
+                
+                if ( this.optionSelected == "firstName" )
+                {
+                    node.send(msg.payload.firstName)
+                    //node.warn(msg.payload.firstName)
+                }
+                else if ( this.optionSelected == "middleName"  )
+                {
+                    node.send(msg.payload.middleName)
+                    //node.warn(msg.payload.middleName)
+                }
+                else if ( this.optionSelected == "lastName"  )
+                {
+                    node.send(msg.payload.lastName)
+                    //node.warn(msg.payload.lastName)
+                }
+                else if ( this.optionSelected == "fullName" )
+                {
+                    node.send( msg.payload.firstName + " " + msg.payload.middleName + " " + msg.payload.lastName )
+                    //node.warn( msg.payload.firstName + " " + msg.payload.middleName + " " + msg.payload.lastName )
+                }
+                //node.warn(this.optionSelected)
+                //node.send(msg);
+                
+            }
+            catch(err) {
+                node.warn(err);
+            }
+
         });
 
         this.on("close", function() {
@@ -63,6 +93,6 @@ module.exports = function(RED) {
 
     // Register the node by name. This must be called before overriding any of the
     // Node functions.
-    RED.nodes.registerType("sample",SampleNode);
+    RED.nodes.registerType("sanitizer",SanitizerNode);
 
 }
